6c7c8205a58640ed3fefdc0ac2a8e25725004fb9
TheBigH50 <HFangmeier@outlook.com>
2022-09-23 10:48:33 -0500

    write supposed mutli input field

--- src/server/controllers/example.controller.js
+++ src/server/controllers/example.controller.js
@@ -3,3 +3,14 @@ import query from "../db/utils";
 export const findAll = async () => {
   return await query("SELECT * FROM table");
 };
+
+/*export const find = async (columnMatch1, columnMatch2, columnMatch3) => {
+  if (columnMatch1 && columnMatch2 && columnMatch3) {
+    return await query("SELECT * FROM products WHERE ? AND ? AND ? ;");
+  } else if (columnMatch1 && columnMatch2 && columnMatch3 == undefined) {
+    return await query("SELECT * FROM products WHERE ? AND ? ;");
+  } else {
+    return await query("SELECT * FROM products WHERE ? ;");
+  }
+};
+*/
