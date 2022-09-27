50526b0524a2dedb3da1ba788beb404adf7d0230
TheBigH50 <HFangmeier@outlook.com>
2022-09-27 11:17:15 -0500

    configure and test servers

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   let [testState, setTestState] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/test")
+    fetch("http://localhost:5000/api/test")
       .then((res) => res.text())
       .then((msg) => setTestState(msg))
       .catch((err) => {
